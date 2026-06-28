4b789b811c1c6cb2673e8ad46ac34f098f4a25f0
Esmien <42712054+Esmien@users.noreply.github.com>
2026-06-28 21:10:25 +0300

    feat: настроена базовая обвязка и подключение к боту (#1)
    * feat: настроено подключение к боту
    
    * dev: временно отключена джоба с тестами

--- src/bot/core/config.py
+++ src/bot/core/config.py
@@ -10,15 +10,27 @@ class LoggerConfig(BaseModelConfig):
     LOG_SERIALIZE: bool = False  # Если True, будет писать в JSON
 
 
+class ApiConfig(BaseModelConfig):
+    API_BASE_URL: str
+
+
 class BotConfig(BaseModelConfig):
-    BOT_TOKEN: str = ""
+    BOT_TOKEN: str
     BOT_HOST: str = "0.0.0.0"
     BOT_PORT: int = 8080
+
+
+class WebhookConfig(BaseModelConfig):
+    WEBHOOK_HOST: str
+    WEBHOOK_PATH: str = "/webhook"
+    WEBHOOK_SECRET: str
 
 
 class Settings(BaseModelConfig):
     logger: LoggerConfig = LoggerConfig()
     bot: BotConfig = BotConfig()
+    webhook: WebhookConfig = WebhookConfig()
+    api: ApiConfig = ApiConfig()
 
 
 settings = Settings()
